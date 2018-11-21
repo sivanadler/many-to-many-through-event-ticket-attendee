69c8ed32b64a5f39e281ee26af300fcb55e90ef3
Chris Metzger <chrismetzgr@gmail.com>
2018-11-21 15:22:35 -0500

    Update READMEs and deliverables

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,8 +3,6 @@ end
 
 # Attendee.all
   # Returns an array of all Attendees
-# Attendee.all_events
-  # Returns an array of all Events that all Attendees have tickets for. An Event instance should not appear multiple times. 
 # Attendee#events
   # Returns an array of all Events that the Attendee is attending.
 # Attendee#money_spent_on_tickets
