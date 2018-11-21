3574ff335e1e4bf9fe307ff28cc3cb48061e1550
Chris Metzger <chrismetzgr@gmail.com>
2018-11-21 16:48:49 -0500

    Add example to console

--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,11 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#Example of a variable definition that will be available in your Pry session once you've built out the model:
+
+lucy = Attendee.new("Lucy", 22)
+
+#The variable `lucy` will point to an instance of a new attendee
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
